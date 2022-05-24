9feddc33b17330993c3530edb178156333ae8d2e
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2022-05-24 14:00:14 -0300

    Coloqué el botón COMPRAR, el cual al ser clickeado emite un mensaje por consola

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -11,6 +11,9 @@ const ItemCount = ({stock}) => {
             <p className="p-1 text-8xl">{count}</p>
             <button onClick={()=>{setCount(count + 1)}} disabled={count === stock} className="p-1 text-8xl text-blue-600"> + </button>
           </div>
+          <div className='flex justify-center'>
+            <button onClick={() => {console.log(`Usted acaba de comprar ${count} empanadas de las mejores del mercado....sépase afortunad@`)}} disabled={count === 0} className='font-bold text-xl border-4 border-slate-600 rounded-lg p-1 bg-orange-400 hover:scale-110 hover:bg-slate-400 hover:text-fuchsia-800'>COMPRAR</button>
+          </div>
         </>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,8 +5,8 @@ import { ItemListContainterTitulo, ContenidoGreeting } from './ItemListContainer
 function ItemListContainer({greeting}) {
     return (
         <>
-          <ItemListContainterTitulo className='p-5'>
-            <div className='border-solid border-2 border-indigo-600 my-auto p-5'>
+          <ItemListContainterTitulo>
+            <div className='border-solid border-2  my-auto p-5'>
               <ContenidoGreeting>Bienvenidos al {greeting} !!!</ContenidoGreeting>
               <ItemCount 
               stock={5}/>
